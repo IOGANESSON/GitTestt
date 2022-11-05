bc129abfecb9351352d811439b11bf68f52504fb
IOGANESSON <112421290+IOGANESSON@users.noreply.github.com>
2022-11-05 21:00:59 +0330

    replaced every thing with last week project to submit in week6

---
+++ src/index.js
@@ -0,0 +1,63 @@
+function dateFormat(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+let city = "london";
+
+function showTemperature(response) {
+  let temp = Math.round(response.data.main.temp);
+  console.log(temp);
+  let h1 = document.querySelector("#temperature");
+  h1.innerHTML = `${temp}`;
+}
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = `c8784affc7f48d97a3ffacdb8dec2d4c`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+  console.log(apiUrl);
+}
+function Submit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = dateFormat(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", Submit);
+
+searchCity("Tehran");
